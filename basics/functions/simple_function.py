61115a4072ce99fa1e734d39ce85854b30bc30dc
Joymayaki <5mayaj72@solent.ac.uk>
2021-03-07 18:24:20 +0100

    "Added code to demonstrate the use of a user-defined function."

---
+++ basics/functions/simple_function.py
@@ -0,0 +1,13 @@
+# The function
+def listen():
+
+    # Ask user for the sound = rumble
+    print("What sound did I hear?")
+    sound = input()
+
+    # Display message
+    print(f"\nThat was a loud {sound}!")
+
+
+# Call to function
+listen()
