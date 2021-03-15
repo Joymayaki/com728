99404eca679e6c12fcad88b73febc2ed5b6b7d73
Joymayaki <5mayaj72@solent.ac.uk>
2021-03-15 13:37:10 +0100

    "Added code to demonstrate a simple list."

---
+++ data/lists/simple_list.py
@@ -0,0 +1,11 @@
+# The function
+def directions():
+  directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+  return directions
+
+# call to function
+def run():
+  print(directions())
+
+if __name__ == "__main__":
+    run()
