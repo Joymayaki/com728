e5fc8ac5ae64a540aea14f763aba5ee2f5a72cef
Joymayaki <5mayaj72@solent.ac.uk>
2021-03-15 18:07:21 +0100

    "Added code to demonstrate how to read a text file.".

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,21 +1,25 @@
+# The function with 2 parameters named file_path, num_chars
 def display_chars(file_path, num_chars):
     with open(file_path) as file:
         data = file.read(num_chars)
         print(data)
 
 
+# The function with a single parameter named 'file_path'
 def display_line(file_path):
     with open(file_path) as file:
         data = file.readline().strip()
         print(data)
 
 
+# The function with a single parameter named 'file_path'
 def display_text(file_path):
     with open(file_path) as file:
         data = file.read()
         print(data)
 
 
+# Call to function
 def run():
     display_chars("library.txt", 5)
     display_line("library.txt")
