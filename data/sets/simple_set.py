f851673acc9011fa4d3b96ee10e2bea4e0effe1c
Joymayaki <5mayaj72@solent.ac.uk>
2021-03-16 17:57:49 +0100

    "Added code to demonstrate a simple set."

---
+++ data/sets/simple_set.py
@@ -0,0 +1,14 @@
+# The function
+def observed():
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+
+# The function
+def run():
+    observations = observed()
+    print(observations)
+
+
+if __name__ == "__main__":
+    run()
