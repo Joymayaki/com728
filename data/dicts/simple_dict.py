8d73b12a8fc4470f95c476c3ac72f4941f14a7b5
Joymayaki <5mayaj72@solent.ac.uk>
2021-03-17 20:36:11 +0100

    "Added code to demonstrate a simple dictionary."

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,14 @@
+# The function
+def pattern():
+    sequences = {"Short Sequence": 3, "Medium Sequence": 2, "Long Sequence": 1}
+    return sequences
+
+
+# The function
+def run():
+    data = pattern()
+    print(data)
+
+
+if __name__ == "__main__":
+    run()
